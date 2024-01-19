48b6392248e8bf5637680ab519d06a2d65775b0f
sinka.jozsef <sinka.jozsef@CAD2-13>
2024-01-19 11:05:38 +0100

    Add project files.

---
+++ WpfApp4/Bekeres.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for Bekeres.xaml
+    /// </summary>
+    public partial class Bekeres : Window
+    {
+        Ujdiak hhh;
+        public Bekeres()
+        {
+            InitializeComponent();
+
+            List<TextBox> textBoxes = new List<TextBox>();
+            textBoxes.Add(txtOm);
+            textBoxes.Add(txtDatum);
+            textBoxes.Add(txtNev);
+            textBoxes.Add(txtEmail);
+            textBoxes.Add(txtCim);
+            textBoxes.Add(txtMatek);
+            textBoxes.Add(txtMagyar);
+        }
+
+        public Bekeres(Ujdiak aa):this()
+        {
+            this.hhh = aa;
+        }
+
+        private void txtMagyar_LostFocus(object sender, RoutedEventArgs e)
+        {
+            hhh.OM_Azonosito = txtOm.Text;
+            hhh.Neve = txtNev.Text;
+            hhh.Email = txtEmail.Text;
+            hhh.SzuletesiDatum = DateTime.Parse(txtDatum.Text);
+            hhh.ErtesitesiCime = txtCim.Text;
+            hhh.Magyar = Convert.ToInt32(txtMagyar.Text);
+            hhh.Matematika = Convert.ToInt32(txtMatek.Text);
+            Close();
+
+        }
+
+    }
+}
---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+
+
+        ObservableCollection<Ujdiak> diakok = new ObservableCollection<Ujdiak>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Importalas_Click(object sender, RoutedEventArgs e)
+        {
+            if (diakok.Count > 0)
+            {
+                ShowDialog();
+            }
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "txt vagy csv fajlok (*.txt;*.csv)|*.txt;*.csv";
+            if (ofd.ShowDialog() == true)
+            {
+                foreach (string fajl in File.ReadAllLines(ofd.FileName).Skip(1))
+                {
+                    diakok.Add(new Ujdiak(fajl.Split(";")));
+                }
+            }
+
+            dgLista.ItemsSource = diakok;
+        }
+
+        private void Torles_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in dgLista.SelectedItems)
+            {
+                diakok.Remove((Ujdiak)item);
+            }
+        }
+
+        private void Felvetel_Click(object sender, RoutedEventArgs e)
+        {
+            Ujdiak asd = new Ujdiak();
+            Bekeres ujablak = new Bekeres(asd);
+            ujablak.Show();
+            diakok.Add(asd);
+            dgLista.ItemsSource = diakok;
+        }
+    }
+}
